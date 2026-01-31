8f708269c82ae062a693d95be84ddd083ff45a69
Touu <hampitou007@gmail.com>
2026-01-31 14:45:22 +0700

    run migrations on render

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,7 @@ php artisan config:cache || true
 php artisan route:cache  || true
 php artisan view:cache   || true
 
+php artisan migrate --force || true
+
 # Start the services (nginx+php-fpm) from the base image
 exec /start.sh
