1011f72daf9322049805138ba77cc8c01c525aad
Touu <hampitou007@gmail.com>
2026-01-31 14:13:32 +0700

    chore: add render docker deploy

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ php artisan config:cache || true
 php artisan route:cache  || true
 php artisan view:cache   || true
 
-# If you use database migrations on deploy, uncomment:
+# If you want auto-migrations on every deploy, uncomment:
 # php artisan migrate --force || true
 
 /start.sh.original
